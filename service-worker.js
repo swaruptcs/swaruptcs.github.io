44a0efb2f23999e7140ae8719b10f77be7049b48
swaruptcs <39014587+swaruptcs@users.noreply.github.com>
2020-07-20 19:16:57 +0100

    Updated title

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84b1afe3189f65f75ca08fec04b8ff1d.js"
+  "/precache-manifest.c9b1f67633366fc033759127395ec3f9.js"
 );
 
 self.addEventListener('message', (event) => {
